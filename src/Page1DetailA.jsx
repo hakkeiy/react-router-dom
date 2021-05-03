6d8c8dbc27ae12a9c10ee9c718c4b69894dd46ae
hakkeiy <38604374+hakkeiy@users.noreply.github.com>
2021-05-03 18:55:55 +0900

    do2

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,4 +1,8 @@
+import { uselocation } from "react-router-dom";
+
 export const Page1DetailA = () => {
+  const location = uselocation;
+  console.log(location);
   return (
     <div>
       <h1>Page1DetailAです。</h1>
--- src/URLParameter.jsx
+++ src/URLParameter.jsx
@@ -1,11 +1,15 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 export const URLParameter = () => {
   const { id } = useParams();
+  const { search } = useLocation();
+  const query = new URLSearchParams(search);
+  console.log(search);
   return (
     <div>
       <h1>URLParameterです。</h1>
       <p>パラメーターは{id}です。</p>
+      <p>パラメーターは{query.get("name")}です。</p>
     </div>
   );
 };
