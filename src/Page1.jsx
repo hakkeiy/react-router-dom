64baf522759652a240ea75a754730bb0a3f65d32
hakkeiy <38604374+hakkeiy@users.noreply.github.com>
2021-05-03 18:43:05 +0900

    do

---
+++ src/Page1.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+
+export const Page1 = () => {
+  return (
+    <div>
+      <h1>Page1です。</h1>
+      <Link to="/page1/detailA">Page1DetailA</Link>
+      <br />
+      <Link to="/page1/detailB">Page1DetailB</Link>
+      <br />
+    </div>
+  );
+};
---
+++ src/router/Router.jsx
@@ -0,0 +1,46 @@
+import { Route, Switch } from "react-router-dom";
+import { Home } from "../Home";
+import { Page1Route } from "./Page1Routes";
+import { Page2Route } from "./Page2Routes";
+
+export const Router = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route
+        path="/page1"
+        render={({ match: { url } }) => (
+          <Switch>
+            {Page1Route.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      ></Route>
+      <Route
+        path="/page2"
+        render={({ match: { url } }) => (
+          <Switch>
+            {Page2Route.map((route) => (
+              <Route
+                key={route.path}
+                exact={route.exact}
+                path={`${url}${route.path}`}
+              >
+                {route.children}
+              </Route>
+            ))}
+          </Switch>
+        )}
+      ></Route>
+    </Switch>
+  );
+};
